1b377c60b92c8b147f4d50c342936ff979057cbe
ushankavinda <ushanrokcy@gmail.com>
2014-08-01 19:20:29 +0530

    User class created

--- public/index.php
+++ public/index.php
@@ -1,5 +1,20 @@
 <?php
+require_once("../includes/database.php");
+require_once("../includes/user.php");
+if(isset($database)) {echo "true";} else {echo "false"; }
 
-echo "Done!";
+echo "<br/>";
 
+//$sql = "INSERT INTO users (id,username,password,first_name,last_name)";
+//$sql .= "VALUES (1,'ushan','ushan123','Ushan','Kavinda')";
+//$result = $database->query($sql);
+
+$sql = "SELECT * FROM users WHERE id = 1";
+$result_set = $database->query($sql);
+$found_user = $database->fetch_array($result_set);
+echo $found_user['username'];
+
+echo "<hr/>";
+$found_user = User::find_by_id(1);
+echo $found_user['username'];
 ?>
