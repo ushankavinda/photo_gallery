9395ef60548b5a0ed6433a4e79834177e783bd1f
ushankavinda <ushanrocky@gmail.com>
2015-05-07 17:26:50 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,18 +1,3 @@
 <?php
-require_once("../includes/database.php");
-require_once("../includes/functions.php");
-//require_once("../includes/user.php");
-
-
-$record = User :: find_by_id(1);
-
-//echo $user->full_name();
-
-echo "<hr/>";
-
-$users = User :: find_all();
-foreach($users as $user) {
-    echo "User: " . $user->username . "<br/>";
-    echo "Name: " . $user->full_name() . "<br/>";
-}
+echo "hello";
 ?>
